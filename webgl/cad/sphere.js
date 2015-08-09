f00a04d80d026aabd36478e2be7903b417f392e4
Martin Velicky <martin@velicky.com>
2015-08-09 12:05:54 +0100

    Grouping drawing logic per-object (shape).

--- webgl/cad/sphere.js
+++ webgl/cad/sphere.js
@@ -2,24 +2,30 @@
 var canvas;
 var gl;
 
-var pointsArray = [];
-var colors = [];
+var program;
+var objects = [];
 
 var near = 0.1;
 var far = 20;
 
+var eye = vec3(0, 10, 0);
+var at = vec3(5, 5, 5);
+var up = vec3(0.0, 1.0, 0.0);
+
 var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 
-var fill = "mesh";
-
+var RED_COLOR = [ 1.0, 0.0, 0.0, 1.0 ];
 var GREEN_COLOR = [ 0.0, 1.0, 0.0, 1.0 ];
 var BLUE_COLOR = [ 0.0, 0.0, 1.0, 1.0 ];
+var MAGENTA_COLOR = [ 1.0, 0.0, 1.0, 1.0 ];
+var CYAN_COLOR = [ 0.0, 1.0, 1.0, 1.0 ];
+var YELLOW_COLOR = [ 1.0, 1.0, 0.0, 1.0 ];
 
-function triangle(a, b, c, color) {
-    pointsArray.push(a);
-    pointsArray.push(b);
-    pointsArray.push(c);
+function triangle(a, b, c, color, vertices, colors) {
+    vertices.push(a);
+    vertices.push(b);
+    vertices.push(c);
 
     colors.push(color);
     colors.push(color);
@@ -30,9 +36,11 @@ function createSphere() {
 
     var latitudeBands = 30;
     var longitudeBands = 30;
-    var radius = 2;
+    var radius = 1;
 
     var grid = [];
+    var vertices = [];
+    var colors = [];
 
     for (var lat = 0; lat <= latitudeBands; lat++) {
 
@@ -54,14 +62,16 @@ function createSphere() {
 
             var currentVertex = vec4(x, y, z, 1.0);
 
+            var color = lat % 2 == 0 && long % 2 == 0 ? GREEN_COLOR : BLUE_COLOR;
+
             if (lat > 0 && long > 0) {
 
                 var previousLatVertex = grid[lat - 1][long];
                 var previousLongVertex = row[long - 1];
                 var diagonalVertex = grid[lat - 1][long - 1];
 
-                triangle(diagonalVertex, previousLongVertex, previousLatVertex, GREEN_COLOR);
-                triangle(previousLongVertex, currentVertex, previousLatVertex, BLUE_COLOR);
+                triangle(diagonalVertex, previousLongVertex, previousLatVertex, color, vertices, colors);
+                triangle(previousLongVertex, currentVertex, previousLatVertex, color, vertices, colors);
             }
 
             row[long] = currentVertex;
@@ -69,9 +79,18 @@ function createSphere() {
 
         grid[lat] = row;
     }
+
+    return {
+        translation: translate(5, 5, 5),
+        colors: colors,
+        vertices: vertices
+    }
 }
 
 function createCube() {
+
+    var vertices = [];
+    var colors = [];
 
     var a = vec4(-0.5, -0.5,  0.5, 1.0),
         b = vec4(-0.5,  0.5,  0.5, 1.0),
@@ -82,23 +101,29 @@ function createCube() {
         g = vec4( 0.5,  0.5, -0.5, 1.0),
         h = vec4( 0.5, -0.5, -0.5, 1.0);
 
-    triangle(a, b, c, GREEN_COLOR);
-    triangle(a, c, d, BLUE_COLOR);
+    triangle(a, b, c, RED_COLOR, vertices, colors);
+    triangle(a, c, d, RED_COLOR, vertices, colors);
 
-    triangle(e, f, b, GREEN_COLOR);
-    triangle(e, b, a, BLUE_COLOR);
+    triangle(e, f, b, GREEN_COLOR, vertices, colors);
+    triangle(e, b, a, GREEN_COLOR, vertices, colors);
 
-    triangle(e, f, g, GREEN_COLOR);
-    triangle(e, g, h, BLUE_COLOR);
+    triangle(e, f, g, BLUE_COLOR, vertices, colors);
+    triangle(e, g, h, BLUE_COLOR, vertices, colors);
 
-    triangle(d, c, g, GREEN_COLOR);
-    triangle(d, g, h, BLUE_COLOR);
+    triangle(d, c, g, CYAN_COLOR, vertices, colors);
+    triangle(d, g, h, CYAN_COLOR, vertices, colors);
 
-    triangle(b, f, g, BLUE_COLOR);
-    triangle(b, g, c, GREEN_COLOR);
+    triangle(b, f, g, YELLOW_COLOR, vertices, colors);
+    triangle(b, g, c, YELLOW_COLOR, vertices, colors);
 
-    triangle(a, h, e, BLUE_COLOR);
-    triangle(a, e, d, GREEN_COLOR);
+    triangle(a, h, e, MAGENTA_COLOR, vertices, colors);
+    triangle(a, e, d, MAGENTA_COLOR, vertices, colors);
+
+    return {
+        translation: translate(9, 4, 4),
+        colors: colors,
+        vertices: vertices
+    }
 }
 
 window.onload = function init() {
@@ -112,27 +137,11 @@ window.onload = function init() {
 
     gl.enable(gl.DEPTH_TEST);
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    //createSphere();
-    createCube();
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray( vPosition);
-
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
-
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor );
+    objects.push(createSphere());
+    objects.push(createCube());
 
     modelViewMatrixLoc = gl.getUniformLocation( program, "mvMatrix" );
     projectionMatrixLoc = gl.getUniformLocation( program, "pMatrix" );
@@ -144,32 +153,35 @@ function render() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var eye = vec3(0, 10, 0);
-    var at = vec3(5, 5, 5);
-    var up = vec3(0.0, 1.0, 0.0);
-
-    modelViewMatrix = lookAt(eye, at , up);
-
-    var t = translate(5, 5, 5);
-
-    modelViewMatrix = mult(modelViewMatrix, t);
-
     projectionMatrix = perspective(45, canvas.width / canvas.height, near, far);
 
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+    for (var o = 0; o < objects.length; o++) {
 
-    // clear & render
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    if (fill === "mesh") {
-        for (var i = 0; i < pointsArray.length; i += 3) {
-            gl.drawArrays( gl.LINE_LOOP, i, 3 );
-        }
-    } else {
-        gl.drawArrays( gl.TRIANGLES, 0, pointsArray.length );
+        var object = objects[o];
+
+        modelViewMatrix = lookAt(eye, at , up);
+
+        gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(object.vertices), gl.STATIC_DRAW);
+
+        var vPosition = gl.getAttribLocation( program, "vPosition");
+        gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray( vPosition);
+
+        var cBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(object.colors), gl.STATIC_DRAW );
+
+        var vColor = gl.getAttribLocation( program, "vColor" );
+        gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vColor );
+
+        modelViewMatrix = mult(modelViewMatrix, object.translation);
+        gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+
+        gl.drawArrays( gl.TRIANGLES, 0, object.vertices.length );
     }
-}
-
-function renderObject() {
-
 }
